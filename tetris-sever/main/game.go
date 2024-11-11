17a70664e1b41f039665fb8b0a7a12cb5615c9f0
timmy <timy3260@gmail.com>
2024-11-11 21:03:06 +0800

    新增簡單註解

--- tetris-sever/main/game.go
+++ tetris-sever/main/game.go
@@ -17,42 +17,52 @@ func update_game_status(Player int, Move string, curGameState GameState) GameSta
 	return curGameState
 }
 
+//執行方塊下落一格
 func Move_Down(Player int, curGameState GameState) GameState {
 	return curGameState
 }
 
+//執行方塊左移一格
 func Move_Left(Player int, curGameState GameState) GameState {
 	return curGameState
 }
 
+//執行方塊右移一格
 func Move_Right(Player int, curGameState GameState) GameState {
 	return curGameState
 }
 
+//執行方塊旋轉
 func Rotate(Player int, curGameState GameState) GameState {
 	return curGameState
 }
 
+//執行方塊快速下降
 func Soft_Drop(Player int, curGameState GameState) GameState {
 	return curGameState
 }
 
+//執行方塊瞬間下降
 func Hard_Drop(Player int, curGameState GameState) GameState {
 	return curGameState
 }
 
+////執行暫存方塊
 func Hold(Player int, curGameState GameState) GameState {
 	return curGameState
 }
 
-func Check_collision(Player int, move string, curGameState GameState) bool {
+//檢查方塊是否衝突
+func Check_collision(Player int, block_type string, block_pos Pos, curGameState GameState) bool {
 	return true
 }
 
+//檢查row有沒有填滿
 func Check_row_full(curGameState GameState) bool {
 	return true
 }
 
+//消除row
 func Eliminate_rows(curGameState GameState) bool {
 	return true
 }
